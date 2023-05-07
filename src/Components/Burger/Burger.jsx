4f3ca304528c4a81dfaa8ab184e4a59389e2e346
Josey Sekine <ipho.7johero@icloud.com>
2023-05-06 22:00:39 -0700

    Responsible and humburger menu

---
+++ src/Components/Burger/Burger.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import {FaBars} from 'react-icons/fa'
+import {AiOutlineClose} from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+import { SidebarData } from './Sidebardata';
+import './burger.css';
+
+function Burger() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <div className="burger_bars">
+        <FaBars onClick={showSidebar}/>
+      </div>
+      <nav className={sidebar ? 'nav_menu active' : 'nav_menu'}>
+        <ul className='nav_menu_items'>
+          <li className='navbar_toggle'>
+            <Link to="#" className='menu_bars'>
+              <AiOutlineClose onClick={showSidebar}/>
+            </Link>
+          </li>
+          {
+            SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Burger
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,20 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
+import Burger from '../Burger/Burger';
 import { AuthContext } from '../../Context/auth-context';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = (props) =>{
+  const [mobile, setMobile] = useState(false);
+
+
+  useEffect(() =>{
+    if(window.innerWidth < 700 ){
+      setMobile(true)
+    }
+  },[])
+
+
   const handleSignOut = (e) =>{
     e.preventDefault();
     auth.logout();
@@ -15,19 +26,30 @@ const Navbar = (props) =>{
 
   return (
     <div id='navbar'>
-      <nav className="nav_links">
-        <Link className="nav_title" to="/">Near</Link>
-        { auth.isLoggedIn?
-          <>
+      <nav className="navbar_left">
+        <div>
+          <Link className="nav_title" to="/">Near</Link>
+        </div>
+        <div className="navbar_contents">
+          <Link to="/signin">Near works</Link>
+          <Link to="/signin">Near categories</Link>
+          <Link to="/signin">How to use</Link>
+        </div>
+      </nav>
+
+      <nav className="navbar_right">
+        { (auth.isLoggedIn && !mobile)?
+          <div className="navbar_contents">
             <Link to="/signout" onClick={handleSignOut}>Signout</Link>
             <Link to={userUrl}>User</Link>
             <p>Hello User!</p>
-          </>
+          </div>
           :
-          <>
+          <div className="navbar_contents">
             <Link to="/signin">Signin</Link>
             <Link to="/login">Login</Link>
-          </>
+            <Burger/>
+          </div>
         }
       </nav>
     </div>
